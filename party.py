06cda438342a0cf526dae49b2e29cba05fd3ac68
Subha Mannargudi <subha.ms@gmail.com>
2020-10-19 20:12:14 +0000

    Completed coding and tested

--- party.py
+++ party.py
@@ -21,9 +21,16 @@ def is_mel(name, email):
     >>> is_mel('Mell', 'mel@ubermelon.com')
     True
 
+    >>> is_mel('MEL', 'mel@ubermelon.com')
+    True
+    
+    >>> is_mel('MEL', 'mel@ubermelon.coM')
+    True
+    >>> 
+
     """
 
-    return name == "Mel Melitpolski" or email == "mel@ubermelon.com"
+    return name.lower() == "mel melitpolski" or name.lower() == "mel" or email.lower() == "mel@ubermelon.com"
 
 
 def most_and_least_common_type(treats):
